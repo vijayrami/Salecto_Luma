d8f908620a75bf882178e0b662176e4416bd6de7
vijayrami <vijaymrami@gmail.com>
2021-02-25 20:29:57 +0530

    First Commit

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+ \Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/Salecto/luma_child',
+    __DIR__
+ );
---
+++ requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    map: {
+        '*' : {
+            'slick' : 'js/slick.min'
+        }
+    },
+    deps: [
+        "js/theme-js"
+    ]
+};
